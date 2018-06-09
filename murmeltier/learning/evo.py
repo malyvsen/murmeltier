07fe46e5c9de25d2233ee7fc05d1ad3123223f54
Nicholas Bochenski <iceflamecode@gmail.com>
2018-06-09 10:49:00 +0200

    default weighter not only numerically stable, but also happens to work
    Ahem. As if that was ever a concern.

--- murmeltier/learning/evo.py
+++ murmeltier/learning/evo.py
@@ -16,10 +16,9 @@ class Evo:
     def train(self, num_epochs = 128, population_size = 64, stddev = 1.0, learning_rate = 0.5, weighter = None, verbosity = 2):
         if weighter is None:
             def weighter(rewards):
-                rms = np.sqrt(np.mean(np.square(rewards)))
-                if rms == 0:
-                    return rewards + 1
-                return np.exp(rewards / rms)
+                factor = 128.0
+                squished_rewards = rewards * (factor / (factor + np.std(rewards)))
+                return np.exp(squished_rewards)
 
         bubbles = [Bubble(env_name = self.env_name) for i in range(population_size)]
 
