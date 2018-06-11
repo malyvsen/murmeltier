ba16143b592e4c83161b24ce1cf084d21bc97a6e
Nicholas Bochenski <iceflamecode@gmail.com>
2018-06-11 12:38:42 +0200

    identity activation

--- murmeltier/units/activations/__init__.py
+++ murmeltier/units/activations/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ['leaky_relu', 'relu', 'soft_exp']
+__all__ = ['identity', 'leaky_relu', 'relu', 'soft_exp']
 
 
+from .identity import Identity
 from .leaky_relu import LeakyReLU
 from .relu import ReLU
 from .soft_exp import SoftExp
---
+++ murmeltier/units/activations/identity.py
@@ -0,0 +1,16 @@
+import numpy as np
+from ..unit import Unit
+from murmeltier.utils import assert_equal_or_none
+
+
+class Identity(Unit):
+    '''
+    Lets values pass without modifying them
+    '''
+    def __init__(self, in_specs = None, out_specs = None):
+        in_specs = out_specs = assert_equal_or_none(in_specs, out_specs)
+        config(in_specs = in_specs, out_specs = out_specs)
+
+
+    def get_output(self, input):
+        return input
