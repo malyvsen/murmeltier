5cd41ce9dbbfb759b9f663b46be262d2933237cc
Nicholas Bochenski <iceflamecode@gmail.com>
2018-06-11 10:36:10 +0200

    utils.trimmed_dict

--- murmeltier/utils/__init__.py
+++ murmeltier/utils/__init__.py
@@ -1,6 +1,7 @@
-__all__ = ['asserts', 'if_print', 'demo', 'spaces']
+__all__ = ['asserts', 'if_print', 'demo', 'spaces', 'trimmed_dict']
 
 
 from .asserts import assert_one, assert_disjoint, assert_keys, assert_equal_or_none
 from .if_print import if_print
 from .demo import demo
+from .trimmed_dict import trimmed_dict
