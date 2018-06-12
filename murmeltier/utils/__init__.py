59ea5d0ab2a2cdbc37d43350fd6496841abf9efe
Nicholas Bochenski <iceflamecode@gmail.com>
2018-06-12 21:03:42 +0200

    utils.curry

--- murmeltier/utils/__init__.py
+++ murmeltier/utils/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ['asserts', 'if_print', 'demo', 'spaces', 'trimmed_dict']
+__all__ = ['asserts', 'curry', 'if_print', 'demo', 'spaces', 'trimmed_dict']
 
 
 from .asserts import assert_one, assert_disjoint, assert_keys, assert_equal_or_none
+from .curry import curry
 from .if_print import if_print
 from .demo import demo
 from .trimmed_dict import trimmed_dict
