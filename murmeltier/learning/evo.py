b227db83ba4b4b8916e97c2366bddbb4859d35f3
Nicholas Bochenski <iceflamecode@gmail.com>
2018-06-28 18:21:08 +0200

    NaNs get eaten instead of propagating

--- murmeltier/learning/evo.py
+++ murmeltier/learning/evo.py
@@ -26,14 +26,15 @@ class Evo:
                 bubble.agent.initialize(explicit_only = True, init_params = {'memory': {}}) # re-initialize memory with default initializer params, but keep everything else
 
             rewards = np.array([bubble.reward for bubble in bubbles])
-            mean_reward = np.mean(rewards)
-            weights = rewards - mean_reward
+            nan_positions = np.isnan(rewards)
+            mean_reward = np.mean(rewards[np.logical_not(nan_positions)])
             update_vector = self.optimal_agent * 0
-            for i in range(len(bubbles)):
-                update_vector += (bubbles[i].agent - self.optimal_agent) * weights[i]
+            for bubble in bubbles:
+                if not np.isnan(bubble.reward):
+                    update_vector += (bubble.agent - self.optimal_agent) * (bubble.reward - mean_reward)
             self.optimal_agent += update_vector * learning_rate
 
-            if_print(verbosity >= 2, 'Mean reward: ' + str(mean_reward))
+            if_print(verbosity >= 2, 'Mean reward: ' + str(mean_reward) + ', NaN count: ' + str(np.sum(nan_positions)))
             if_print(verbosity >= 1, '')
 
         return self.optimal_agent
